09498b80e3d17df12d56e09dd8756e7f9c210518
unknown <wchomrak@gmail.com>
2021-09-30 09:05:38 -0500

    Configure access credentials

--- reddit-bot.py
+++ reddit-bot.py
@@ -5,6 +5,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-test = os.getenv("TEST")
+CLIENT_ID = os.getenv("PERSONAL_USE_SCRIPT")
+CLIENT_SECRET = os.getenv("SECRET")
+USER_AGENT = os.getenv("USER_AGENT")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 
-reddit = praw.Reddit(client_id="", client_secret="", password="", user_agent="", username="")
+reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT, username=USERNAME, password=PASSWORD)
+
+print(reddit.read_only)
