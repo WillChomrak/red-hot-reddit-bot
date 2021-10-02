9923dd590194bcbae4b9daafc0715cee4f247ccb
unknown <wchomrak@gmail.com>
2021-10-02 12:28:48 -0500

    Troubleshoot and fix access issues

--- reddit-bot.py
+++ reddit-bot.py
@@ -3,14 +3,41 @@
 import praw
 import os
 from dotenv import load_dotenv
+import pprint
 
 load_dotenv()
 CLIENT_ID = os.getenv("PERSONAL_USE_SCRIPT")
 CLIENT_SECRET = os.getenv("SECRET")
 USER_AGENT = os.getenv("USER_AGENT")
-USERNAME = os.getenv("USERNAME")
+R_USERNAME = os.getenv("R_USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 
-reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT, username=USERNAME, password=PASSWORD)
+reddit = praw.Reddit(
+    client_id=CLIENT_ID, 
+    client_secret=CLIENT_SECRET, 
+    user_agent=USER_AGENT, 
+    username=R_USERNAME, 
+    password=PASSWORD,
+)
 
-print(reddit.read_only)
+chicken = reddit.subreddit("chicken")
+print(chicken.display_name)
+print(chicken.title)
+print(reddit.user.me())
+
+# for submission in chicken.hot(limit=10):
+#     print(submission.title)
+#     print(submission.score)
+#     print(submission.id)
+
+
+# So what can I do with this now that its working?
+# can get instances of subreddits and posts within those subreddits
+# instances of authors
+# comments on submissions
+
+pprint.pprint(vars(chicken))
+
+# Maybe I want to make it so the user can dl all the info about whatever subreddit they want.
+# Then give them the option to output that as a csv? That could be cool. Then another project could be to
+# that that info and do some data analysis on it.
